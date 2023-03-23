71a7b88665dd78a7e4a6c55ff21d2297e568d105
AndrewMurphy <amurf26@outlook.com>
2023-03-22 18:20:10 -0700

    notes/activity

--- JQUERY/script.js
+++ JQUERY/script.js
@@ -1,5 +1,15 @@
 $(function(){
 
+    $("#panel2").on("click", function(){
+        $(this).animate({"opacity": '0%'}, 1000).animate({"opacity": '80%'}, 2000);
+    })
+    $("#panel3").on("click", function(){
+        $(this).slideUp(1000).delay(1000).slideDown(1500);
+    })
+    $("#panel4").on("click", function(){
+        $(this).animate({"opacity": '30%'}, 1000).slideUp(2000).slideDown(2000).animate({"opacity": '10%'}, 2000).animate({"opacity": '100%'}, 2000)
+    });
+
     $(".tabs-panels .tabs li").on("click", function(){
         var $panel = $(this).closest('.tabs-panels');
         $panel.find('.tabs li.active').removeClass('active');
