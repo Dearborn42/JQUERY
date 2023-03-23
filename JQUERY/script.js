0f516ee3681e983e09212a9b680a0d5471872299
AndrewMurphy <amurf26@outlook.com>
2023-03-22 17:04:36 -0700

    Added notes

--- JQUERY/script.js
+++ JQUERY/script.js
@@ -1,8 +1,19 @@
-$(window).on("load", function() {
-    $("p").on("click", function(){
-        $(this).html("Hello");
-    }).on("dblclick", function(){
-        $(this).html("World");
-    })
-	$(".templar").animate({left: '300px'});
+$(function(){
+
+    $(".tabs-panels .tabs li").on("click", function(){
+        var $panel = $(this).closest('.tabs-panels');
+        $panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        var panelToShow = $(this).attr('rel');
+
+        $panel.find('.panel.active').slideUp(300, showNext);
+
+        function showNext() {
+            $(this).removeClass('active');
+            $("#"+panelToShow).slideDown(300, function() {
+                $(this).addClass('active');
+            });
+        }
+    });
 });
