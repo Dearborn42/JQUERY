4794942479906b926bba42e69ae2f7bd596088c7
Dearborn42 <amurf26@outlook.com>
2023-04-06 18:19:08 -0700

    started new project

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+$(function () {
+    var width = 720;
+    var animateSpeed = 2000;
+    var pause = animateSpeed + 50;
+    var currentSlide = 1;
+    var $slideContainer=$('.slides')
+})
