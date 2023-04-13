b02b6065f383494c3e10176ecc4f5b6e2491f789
Dearborn42 <amurf26@outlook.com>
2023-04-12 18:20:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,29 @@
 $(function () {
     var width = 720;
     var animateSpeed = 2000;
-    var pause = animateSpeed + 50;
+    var pause = 100;
     var currentSlide = 1;
     var $slideContainer=$('.slides')
     var $slides = $('.slide');
     var interval;
     function startSlide() {
         interval = setInterval(function () {
-            $slideContainer.animate({'margin-left':'-='+width}, animateSpeed)
-        }, animateSpeed);
+            $slideContainer.animate({'margin-left':'-='+width}, 100, 
+            function(){
+                currentSlide++;
+                if (currentSlide === $slides.length){
+                    currentSlide = 1;
+                    $slideContainer.css({'margin-left': 0})
+                }
+            })
+        }, pause);
     }
+
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    startSlide();
+
+    $slideContainer.on('mouseenter', pauseSlider).on('mouseleave', startSlide);
 })
